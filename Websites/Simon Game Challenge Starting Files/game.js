054700419fd11cedad408f3fae7a0b734d85a445
akramaslam2001 <93449016+akramaslam2001@users.noreply.github.com>
2023-12-12 21:36:34 +0530

    Add files via upload

---
+++ Websites/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,87 @@
+var buttonColors =["red","yellow","blue","green"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if(!started){
+        $("h1").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+checkAnswer(userClickedPattern.length-1);
+
+});
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+     if(gamePattern.length===userClickedPattern.length){
+        setTimeout(function() {
+            nextSequence();
+        }, 1000);
+     }
+    }
+    else {
+      playSound("worng");
+      $("body").addClass("game-over");
+      $("h1").text("Game Over,Press Any Key to Restart");
+
+      setTimeout(function(){
+       $("body").removeClass("game-over");
+      },200);
+
+      startOver();     
+    }
+}
+
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("h1").text("Level "+level);
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#"+ randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+
+
+}
+
+function playSound(name){
+    
+    var audio = new Audio("sounds/"+ name +".mp3");
+    audio.play();
+
+}
+
+function animatePress(currentColor){
+
+    $("#"+currentColor).addClass("pressed");
+
+  setTimeout(function() {
+       $("#"+currentColor).removeClass("pressed");
+  },100);
+
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
